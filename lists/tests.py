9c7dbcf34ec7977c955bb0ad0aa4d42746534be9
diego.choi <diego.choi@kakaocorp.com>
2018-05-02 17:50:30 +0900

    Add unique URLs for each list

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,7 @@ from django.template.loader import render_to_string
 from lists.views import home_page
 from lists.models import Item, List
 
+
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
@@ -21,6 +22,7 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
 
         self.assertTemplateUsed(response, 'home.html')
+
 
 class ListAndItemModelTest(TestCase):
 
@@ -51,21 +53,29 @@ class ListAndItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
 
+
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_items(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list itemey 1', list=other_list)
+        Item.objects.create(text='other list itemey 2', list=other_list)
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        response = self.client.get(f'/lists/{correct_list.id}/')
 
-        self.assertContains(response, 'itemey 1')  
+        self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other list itemey 1')
+        self.assertNotContains(response, 'other list itemey 2')
+
 
 class NewListTest(TestCase):
     
@@ -77,5 +87,6 @@ class NewListTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        new_list = List.objects.first()
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
